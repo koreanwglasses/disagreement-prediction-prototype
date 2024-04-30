fbcace227a603df55860218b1222366f4d173a01
koreanwglasses <koreanwglasses@gmail.com>
2024-04-30 16:19:40 -0500

    Replicate reddit layout

--- app/lib/components/modqueue/entry.tsx
+++ app/lib/components/modqueue/entry.tsx
@@ -1,16 +1,108 @@
 import { Entry } from "@/lib/models/modqueue/entries";
-import { Box } from "@mui/material";
+import { Avatar, Box } from "@mui/material";
+import Icon from "@mdi/react";
+import { mdiFlag } from "@mdi/js";
+import _ from "lodash";
 
 export const EntryRenderer = ({ entry }: { entry: Entry }) => {
   return (
     <Box
       sx={{
+        p: 1,
+        m: 1,
         border: 1,
         borderRadius: 1,
+        borderColor: "rgba(0,0,0,0.25)",
+        display: "flex",
+        gap: 1,
       }}
     >
-      <Box component="p" sx={{ opacity: 0.6 }}>{entry.title}</Box>
-      <Box component="p">{entry.text}</Box>
+      <Box width="48px" flexShrink={0} />
+      <Box display="flex" flexDirection="column" gap={1}>
+        <HeaderRenderer entry={entry} />
+        <BodyRenderer entry={entry} />
+        <ReportsRenderer entry={entry} />
+      </Box>
     </Box>
+  );
+};
+
+const HeaderRenderer: React.FC<{
+  entry: Entry;
+}> = ({ entry }) => {
+  const subreddit = "r/changemyview";
+  const timestamp = "0 minutes ago";
+
+  return (
+    <Box
+      display="flex"
+      gap={0.5}
+      color="rgba(0,0,0,0.5)"
+      fontSize="0.75em"
+      alignItems="center"
+    >
+      <Avatar alt={subreddit} src="/cmv.png" sx={{ width: 24, height: 24 }} />
+      <span>{subreddit}</span>
+      <span>·</span>
+      <span>Commented by {entry.author_name}</span>
+      {entry.flair && (
+        <Box bgcolor="#cfcfcf" borderRadius={1} px={0.5} fontWeight="bold">
+          {entry.flair}
+        </Box>
+      )}
+      <span>·</span>
+      <span>{timestamp}</span>
+    </Box>
+  );
+};
+
+const BodyRenderer = ({ entry }: { entry: Entry }) => {
+  return (
+    <>
+      <Box component="p" sx={{ opacity: 0.5 }}>
+        {entry.title}
+      </Box>
+      <Box
+        component="p"
+        sx={{
+          px: 1,
+          borderLeft: 1,
+          borderLeftStyle: "dashed",
+        }}
+      >
+        {entry.text}
+      </Box>
+    </>
+  );
+};
+
+const ReportsRenderer = ({ entry }: { entry: Entry }) => {
+  const numReports = _.sum(Object.values(entry.reports ?? {}));
+  return (
+    entry.reports && (
+      <Box
+        sx={{
+          p: 1,
+          bgcolor: "#ffefcc",
+          display: "flex",
+          borderRadius: 1,
+          gap: 1,
+        }}
+      >
+        <Icon path={mdiFlag} size={1} color="#ffc33d" className="flagIcon" />
+        <Box>
+          <Box component="h3" fontWeight="bold">
+            {numReports} {numReports == 1 ? "Report" : "Reports"}
+          </Box>
+          <Box>
+            {Object.entries(entry.reports).map(([rule, count], i) => (
+              <Box key={i}>
+                {rule} ({count})
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      </Box>
+    )
   );
 };
--- app/lib/models/modqueue/entries.ts
+++ app/lib/models/modqueue/entries.ts
@@ -1,13 +1,21 @@
 import { getDB } from "@/lib/mongodb";
-import { ObjectId } from "mongodb";
+import hash from "string-hash";
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  animals,
+  colors
+} from "unique-names-generator";
+import { WithId } from "mongodb";
 
 export interface Entry {
+  author_name?: string;
   text: string;
   title: string;
   post_body: string;
   time_delay: number;
   flair: string | null;
-  reports: {
+  reports?: {
     [rule: string]: number;
   };
   is_op?: boolean;
@@ -20,6 +28,18 @@ export const getCollection = async () => {
 
 export const getEntries = async () => {
   const collection = await getCollection();
-  const items = await collection.find({}).limit(10).toArray();
-  return items;
+  const items = await collection.find({}).limit(20).toArray();
+  return generateAuthorNames(items);
+};
+
+const generateAuthorName = (entry: Entry) => {
+  const seed = hash(entry.text) ^ hash(entry.title) ^ hash(entry.post_body);
+  return uniqueNamesGenerator({ dictionaries: [adjectives, colors], seed });
+};
+
+const generateAuthorNames = (entries: WithId<Entry>[]) => {
+  return entries.map((entry) => ({
+    ...entry,
+    author_name: generateAuthorName(entry),
+  }));
 };
